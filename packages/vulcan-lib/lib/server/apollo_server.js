516a0c4a155f8f9cca3906d980d4fa3d74fd1170
Bogdan Dimofte <bogdan.dimofte+github@shop.co>
2019-01-02 09:59:45 -0400

    apollo server: file limit of 200mb

--- packages/vulcan-lib/lib/server/apollo_server.js
+++ packages/vulcan-lib/lib/server/apollo_server.js
@@ -131,7 +131,7 @@ const createApolloServer = (givenOptions = {}, givenConfig = {}) => {
   graphQLServer.use(compression());
 
   // GraphQL endpoint
-  graphQLServer.use(config.path, bodyParser.json(), graphqlExpress(async (req) => {
+  graphQLServer.use(config.path, bodyParser.json({limit: '200mb'}), graphqlExpress(async (req) => {
     let options;
     let user = null;
 
